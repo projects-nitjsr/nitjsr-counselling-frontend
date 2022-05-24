194c0faf67432e77cbd2cb288a7fd2b1b557866c
Deepak Kumar <deepakkumar100vvr@gmail.com>
2022-05-24 20:26:27 +0530

    Form Updated

--- nimcet/src/components/registration/signup/index.jsx
+++ nimcet/src/components/registration/signup/index.jsx
@@ -1,37 +1,51 @@
-import './style.css';
 import React from "react";
 import ReCAPTCHA from "react-google-recaptcha";
+import './style.css';
 
 function onChange(value) {
   console.log("Captcha value:", value);
 }
 const signup = () => {
-	<div className='header'>
-          New Candidate Signin
-        </div>
-        <form>
-            <label className='required'>First Name</label>
-            <input className="input-field " placeholder='First Name' type="text"/>
-            <br/>
-            <label>Middle Name</label>
-            <input className="input-field" placeholder='Middle Name' type="text"/>
-            <br/>
-            <label>Last Name</label>
-            <input className="input-field" placeholder='Last Name' type="text"/>
-            <br/>
-            <label className='required'>Email</label>
-            <input className="input-field" placeholder='Email' type="text"/>
-            <br/>
-            <label className='required'>Mobile</label>
-            <input className="input-field" placeholder='Mobile' type="text"/>
-            <br/>
-            <ReCAPTCHA
-              sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
-              onChange={onChange}
-            />
-
-            <button> Reset</button>
-        </form>
+	 return (
+		<div className="App">
+		<div className='header'>New Candidate Signin</div>
+			<form className="form">
+				<div className="form_elem">
+				<label className='required'>First Name</label>
+				<input className="input-field " placeholder='First Name' type="text"/>
+				</div>
+				<br/>
+				<div className="form_elem">
+				<label>Middle Name</label>
+				<input className="input-field" placeholder='Middle Name' type="text"/>
+				</div>
+				<br/>
+				<div className="form_elem">
+				<label>Last Name</label>
+				<input className="input-field" placeholder='Last Name' type="text"/>
+				</div>
+				<br/>
+				<div className="form_elem">
+				<label className='required'>Email</label>
+				<input className="input-field" placeholder='Email' type="text"/>
+				</div>
+				<br/>
+				<div className="form_elem">
+				<label className='required'>Mobile</label>
+				<input className="input-field" placeholder='Mobile' type="text"/>
+				</div>
+				<br/>
+				<div className="form_elem captcha">
+				<ReCAPTCHA
+				sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
+				onChange={onChange}
+				/>
+				</div>
+				<button className="reset-btn"> Reset</button>
+			</form>
+		</div>
+	 )
+	
 };
 
 export default signup;
