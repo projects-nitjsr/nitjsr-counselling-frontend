d98cf6b3ca122a5f46f91860e882660ac7677be9
Chetan Agarwal <chetanagarwal487@gmail.com>
2022-05-25 22:49:01 +0530

    Document Upload & Basic Details done

--- nimcet/src/App.js
+++ nimcet/src/App.js
@@ -1,17 +1,24 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import AdminPage from "./pages/admin";
-import SignIn from "./pages/admin/signin";
-import Reset from "./pages/admin/reset";
+import Councellor from "./pages/councellor";
+import SignIn from "./pages/councellor/signin";
+import Reset from "./pages/councellor/reset";
 import Client from "./pages/client";
 import CandidateSignIn from "./pages/client/signin";
 import CandidateReset from "./pages/client/reset";
 import Home from "./pages/home";
-import Confirmation from "./pages/admin/confirmation/Confirmation";
-import SetPasswords from "./pages/admin/Passwords/Index";
-import Remark from "./pages/admin/remark";
+import Confirmation from "./pages/councellor/confirmation/Confirmation";
+import SetPasswords from "./pages/councellor/Passwords/Index";
+import Remark from "./pages/councellor/remark";
 import Notice from "./pages/home/notice";
 import Download from "./pages/home/download";
+import Information from "./pages/councellor/information/Information";
+import CandidateInfo from "./pages/councellor/CandidateInfo";
+import CenterIncharge from "./pages/centerIncharge";
+import Admin from "./pages/admin";
+import SetPasswordIncharge from "./pages/centerIncharge/setPassword";
+import AddCouncellor from "./pages/centerIncharge/addCouncellor";
+import CandidateDetail from "./pages/centerIncharge/candidateDetail";
 
 function App() {
   return (
@@ -24,13 +31,22 @@ function App() {
           <Route path="signin" element={<CandidateSignIn />} />
           <Route path="reset" element={<CandidateReset />} />
         </Route>
-        <Route path="admin" element={<AdminPage />}>
+        <Route path="center-incharge" element={<CenterIncharge />}>
+          <Route path="set-password" element={<SetPasswordIncharge />} />
+          <Route path="add-councellor" element={<AddCouncellor />} />
+          <Route path="candidate-detail" element={<CandidateDetail />} />
+        </Route>
+        <Route path="councellor" element={<Councellor />}>
           <Route path="signin" element={<SignIn />} />
           <Route path="reset" element={<Reset />} />
+          <Route path="candidate-info" element={<CandidateInfo />} />
+
           <Route path="confirm" element={<Confirmation />} />
           <Route path="setPassword" element={<SetPasswords />} />
           <Route path="remark" element={<Remark />} />
+          <Route path="information" element={<Information />} />
         </Route>
+        <Route path="admin" element={<Admin />}></Route>
       </Routes>
     </BrowserRouter>
   );
