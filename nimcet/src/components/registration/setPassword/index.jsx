c8adb38b43c46511179ad41ec05da4bb01e5e250
thelastpawn009 <72349502+thelastpawn009@users.noreply.github.com>
2022-05-25 18:29:15 +0530

    Update index.jsx

--- nimcet/src/components/registration/setPassword/index.jsx
+++ nimcet/src/components/registration/setPassword/index.jsx
@@ -1,7 +1,39 @@
 import React from "react";
-
+import './style.css';
 const setPassword = () => {
-	return <div>setPassword</div>;
+	return (
+		<div className="outer-box">
+		<div className='header'>Set Your Password?</div>
+			<div className="heading">Account activation for *******</div>
+			<form className="form">
+				<div className="form_elem">
+					<label className='required'>Choose a Password:</label>
+					<input className="input-field "  type="text"/>
+				</div>
+				<br/>
+				<div className="form_elem">
+					<label className="required">Password Confirmation:</label>
+					<input className="input-field"  type="text"/>
+				</div>
+				<br/>
+				<div className="form_elem">
+					<label className="required">Choose a security question:</label>
+					<select className=" select-field">
+						<option value="school-name">What is the name of your elementary school</option>
+						<option value="pet-name">What is your pets name</option>
+					</select>
+				</div>
+				<br/>
+				<div className="form_elem answer-field">
+					<label className="required">Answer:</label>
+					<input className="input-field"  type="text"/>
+				</div>
+				<br/>
+				
+				<button className="activate-btn">Activate</button>
+			</form>
+		</div>
+	)
 };
 
 export default setPassword;
